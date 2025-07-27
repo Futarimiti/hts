bc0358eff23e14033d96d093977fe2607c4c4989
Carman Fu <huivioemp1bs@posteo.com>
2025-07-27 15:10:40 +0800

    flake: override the broken xml-lens package

--- flake.nix
+++ flake.nix
@@ -7,70 +7,53 @@
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          pkgs =
-            nixpkgs.legacyPackages.${system};
-          haskellPackages =
-            pkgs.haskell.packages.ghc9101;
-          findPackages =
-            with builtins;
-            with pkgs.lib;
-            let
-              stringIsNat =
-                str:
-                match "^[0-9]+$" str
-                != null;
-            in
-            dir:
-            readDir dir
-            |>
-              filterAttrs
-                (
-                  name: state:
-                  state
-                  == "directory"
-                  && stringIsNat name
-                )
-            |>
-              mapAttrs
-                (
-                  name: _:
-                  haskellPackages.developPackage
-                    {
-                      root = path.append dir name;
-                    }
-                );
-        in
-        {
-          packages = findPackages ./.;
-          devShells = {
-            default =
-              haskellPackages.shellFor
-                {
-                  packages =
-                    _:
-                    [ ];
-                  nativeBuildInputs = [
-                    pkgs.zlib
-                    pkgs.pkg-config
-                    pkgs.cookiecutter
-                    pkgs.cairo
-                    pkgs.expat
-                    pkgs.xorg.libXdmcp
-                    haskellPackages.cabal-install
-                    haskellPackages.hpack
-                    haskellPackages.haskell-language-server
-                  ];
-                };
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskell.packages.ghc9101;
+        xml-lens = pkgs.fetchFromGitHub {
+          owner = "bellroy";
+          repo = "xml-lens";
+          rev = "41248a393579d27f3b0432714113f159412f028d";
+          sha256 = "sha256-P7oYIDEgmo/eGoPDfULttXq4+fjlQGkJ8hI1t9hh3Hs=";
+        };
+        findPackages =
+          with builtins;
+          with pkgs.lib;
+          let
+            stringIsNat = str: match "^[0-9]+$" str != null;
+          in
+          dir:
+          readDir dir
+          |> filterAttrs (name: state: state == "directory" && stringIsNat name)
+          |> mapAttrs (
+            name: _:
+            haskellPackages.developPackage {
+              root = path.append dir name;
+              source-overrides = { inherit xml-lens; };
+            }
+          );
+      in
+      {
+        packages = findPackages ./.;
+        devShells = {
+          default = haskellPackages.shellFor {
+            packages = _: [ ];
+            nativeBuildInputs = [
+              pkgs.zlib
+              pkgs.pkg-config
+              pkgs.cookiecutter
+              pkgs.cairo
+              pkgs.expat
+              pkgs.xorg.libXdmcp
+              haskellPackages.cabal-install
+              haskellPackages.hpack
+              haskellPackages.haskell-language-server
+            ];
           };
-        }
-      );
+        };
+      }
+    );
 }
