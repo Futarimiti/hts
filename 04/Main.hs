29e6aaee894026e2ec3ca4ac7e200b2070f78b50
Carman Fu <huivioemp1bs@posteo.com>
2025-07-28 12:52:57 +0800

    04: use hts-utils

--- 04/Main.hs
+++ 04/Main.hs
@@ -1,27 +1,15 @@
 module Main where
 
-import Codec.Compression.BZip      qualified as BZip
-import Data.ByteString.Char8       qualified as BS8
-import Network.HTTP.Client         (Request (..), Response (..), httpLbs, parseRequest)
-import Network.HTTP.Client.OpenSSL (newOpenSSLManager)
-import Network.HTTP.Types.Header   (hCookie)
-import Parse                       (readInstructions)
-import Plot                        (plotInstructions)
-import System.Environment.Blank    (getEnv)
-import System.IO                   (hPutStrLn, stderr)
+import Codec.Compression.BZip qualified as BZip
+import HTS.Utils              qualified as HTS
+import Parse                  (readInstructions)
+import Plot                   (plotInstructions)
+import System.IO              (hPutStrLn, stderr)
 
 main :: IO ()
 main = do
-  Just session <- getEnv "HACK_THIS_SITE_SESSION"
-  manager <- newOpenSSLManager
-  -- NOTE the ending slash is significant
-  basicReq <- parseRequest "https://www.hackthissite.org/missions/prog/4/XML/"
-  let request = basicReq
-        { requestHeaders = [(hCookie, "HackThisSite=" <> BS8.pack session)] }
-  response <- httpLbs request manager
-  let
-    bzip = responseBody response
-    xml = BZip.decompress bzip
+  bzip <- HTS.fetch "https://www.hackthissite.org/missions/prog/4/XML/"
+  let xml = BZip.decompress bzip
   instructions <- readInstructions xml
   plotInstructions instructions "output.png"
   hPutStrLn stderr "Saved results to output.png"
--- 04/default.nix
+++ 04/default.nix
@@ -8,5 +8,6 @@ haskell.packages.ghc9101.developPackage {
       rev = "41248a393579d27f3b0432714113f159412f028d";
       sha256 = "sha256-P7oYIDEgmo/eGoPDfULttXq4+fjlQGkJ8hI1t9hh3Hs=";
     };
+    hts-utils = ../hts-utils;
   };
 }
